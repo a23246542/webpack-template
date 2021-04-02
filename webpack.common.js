feb6f354fa718663e9f2a92dc644bf9cc959dfe6
a23246542 <a23246542@gmail.com>
2021-04-03 00:19:11 +0800

    feat: 分割第三分代碼

--- webpack.common.js
+++ webpack.common.js
@@ -70,7 +70,7 @@ module.exports = {
       filename:'index.html', //要生成的檔案
       template:`${srcPath}/html/index.html`, //模板來源
       //chunks 表示該頁面要引用哪些 chunks，默認全部引用
-      chunks: ['index']
+      chunks: ['vendor','index']
     }),
     // new HtmlWebpackPlugin({ //多入口單純產生html，同樣會引入main.js
     //   filename: 'about.html'
@@ -82,9 +82,38 @@ module.exports = {
       chunks:['about'],
     })
   ],
-  // devServer: { //啟動本地服務
-  //   port: 8080,
-  //   contentBase:distPath,//設定當前的目錄是什麼
-  //   open: true, //自動打開瀏覽器
-  // }
+  optimization: {
+    // 壓縮 css
+    // minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+
+    // 分割程式碼模組
+    splitChunks: {
+        // chunks: 'all',
+        /**
+         * initial 入口chunk，對於非同步導入的文件不處理
+            async 分同步chunk，只對非同步導入的文件處理
+            all 全部chunk
+         */
+        // 缓存分组
+        cacheGroups: {
+            // 第三方模組
+            vendor: {
+                test: /node_modules/,
+                name: 'vendor', // chunk 名稱
+                chunks: 'initial',
+                enforce: true,
+                priority: 1, // 權限更高，優先抽離，重要
+                minSize: 0,  // 大小限制
+                minChunks: 1  // 最少複用過幾次
+            },
+            // 公共的模塊
+            // common: {
+            //     name: 'common', // chunk 名稱
+            //     priority: 0, // 優先級
+            //     minSize: 0,  // 公共模塊的大小限制
+            //     minChunks: 2  // 公共模塊最少複用過幾次
+            // }
+        }
+    }
+  }
 }
--- webpack.prod.js
+++ webpack.prod.js
@@ -87,5 +87,39 @@ module.exports = merge(webpackCommonConf, {
         //     // window.ENV = 'production'
         //     ENV: JSON.stringify('production'
         // })
-    ]
+    ],
+    // optimization: {
+    //   // 壓縮 css
+    //   // minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+
+    //   // 分割程式碼模組
+    //   splitChunks: {
+    //       // chunks: 'all',
+    //       /**
+    //        * initial 入口chunk，對於非同步導入的文件不處理
+    //           async 分同步chunk，只對非同步導入的文件處理
+    //           all 全部chunk
+    //        */
+    //       // 缓存分组
+    //       cacheGroups: {
+    //           // 第三方模組
+    //           vendor: {
+    //               test: /node_modules/,
+    //               name: 'vendor', // chunk 名稱
+    //               chunks: 'initial',
+    //               enforce: true,
+    //               priority: 1, // 權限更高，優先抽離，重要
+    //               minSize: 0,  // 大小限制
+    //               minChunks: 1  // 最少複用過幾次
+    //           },
+    //           // 公共的模塊
+    //           // common: {
+    //           //     name: 'common', // chunk 名稱
+    //           //     priority: 0, // 優先級
+    //           //     minSize: 0,  // 公共模塊的大小限制
+    //           //     minChunks: 2  // 公共模塊最少複用過幾次
+    //           // }
+    //       }
+    //   }
+    // }
 })
